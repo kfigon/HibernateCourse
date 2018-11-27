8078e2adfe5063eeeeeeb98a2db2cbcedc8624c3
Kamil Figoń <kamil.figon@jitsolutions.pl>
2018-11-27 13:51:44 +0100

    cleanup

--- _3_advancedHibernate/src/test/java/mypackage/ApplicationTest.java
+++ _3_advancedHibernate/src/test/java/mypackage/ApplicationTest.java
@@ -1,6 +1,11 @@
 package mypackage;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootTest
 public class ApplicationTest {
+
 }
