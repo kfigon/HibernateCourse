69e96f1658b002ea07feb59181e8fff1c3aab106
Kamil Figoń <kamil.figon@gmail.com>
2018-12-14 15:37:47 +0100

    many to many

--- _3_advancedHibernate/src/main/resources/data.sql
+++ _3_advancedHibernate/src/main/resources/data.sql
@@ -1,30 +1,32 @@
--- jpa nie wymaga tworzenia tego schematu
+-- jpa nie wymaga tworzenia schematu recznie (sterowane propertiesem)
 
+-- regular entity
 insert into course(id, name) values(1, 'praktyka programowania');
 insert into course(id, name) values(2, 'matematyka');
 insert into course(id, name) values(3, 'fizyka');
 
+-- one to one unidirection
 insert into student(id, name) values(1, 'Adam');
 insert into student(id, name) values(2, 'Marek');
 
--- passport jest ownerem relacji. Jednokierunkowa relacja
+-- passport jest ownerem relacji (trzyma id drugiego). Tutaj adnotacja
 insert into passport(id, student_id, passport_date) values(1, 1, sysdate());
 insert into passport(id, student_id, passport_date) values(2, 2, sysdate());
 
 
--- dwukierunkowa relacja. owner - Owner. on trzyma dg ID
+-- dwukierunkowa relacja. owner - Owner. on trzyma dog ID
 -- w encji dog trzeba dac wtedy co mapuje go w encji nadrzednej
 insert into dog(id, name) values(1, 'Fafik');
 insert into owner(id, name, dog_id) values(1, 'Jan',1);
 
 
--- one to many jednokierunkowe. post ma wiele komentarzy
+-- one to many jednokierunkowe. post ma wiele komentarzy. Join column potrzebny
 insert into post(id, topic) values (1, 'fizyka kwantowa');
 insert into comment(id, content, post_id) values (1, 'very nice!', 1);
 insert into comment(id, content, post_id) values (2, 'boooring', 1);
 insert into comment(id, content, post_id) values (3, 'show photos pls', 1);
 
--- one to many dwukierunkowe.
+-- one to many dwukierunkowe. Join column potrzebny
 insert into orderx(id, name) values(1, 'stuff');
 insert  into order_item(id, description, order_id) values (1, 'some things', 1);
 insert  into order_item(id, description, order_id) values (2, 'other things', 1);
@@ -33,7 +35,7 @@ insert  into order_item(id, description, order_id) values (3, 'stranger things',
 
 -- many to many
 -- bez mappingu beda 2 join tablice
--- robimy owning (np. book) i dajemy adnotacje a drugiej
+-- robimy owning (np. book) i dajemy adnotacje a drugiej (mappedBy)
 insert into book(id, name) values (1, 'java in practice');
 insert into book(id, name) values (2, 'JPA in practice');
 insert into book(id, name) values (3, 'boring book');
