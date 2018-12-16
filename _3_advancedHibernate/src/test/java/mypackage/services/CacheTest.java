18324aa348620e3cc9e0e57c27f29b9212aec0fb
Kamil Figoń <kamil.figon@gmail.com>
2018-12-16 13:44:42 +0100

    kesz notatki

---
+++ _3_advancedHibernate/src/test/java/mypackage/services/CacheTest.java
@@ -0,0 +1,17 @@
+package mypackage.services;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CacheTest {
+//    pierwszy poziom kesza - persistance context kazdej transakcji
+//  2 ready tego samego nie striggeruja selecta
+
+//    drugi - wszystkie transakcje
+
+//    sterowanie (szczegolnie second level) - EhCache z hibernate
+}
