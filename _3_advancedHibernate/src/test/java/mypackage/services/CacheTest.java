acef7b3b6f9a235d903134287b73db87b2554341
Kamil Figoń <kamil.figon@gmail.com>
2018-12-16 14:17:49 +0100

    test fix

--- _3_advancedHibernate/src/test/java/mypackage/services/CacheTest.java
+++ _3_advancedHibernate/src/test/java/mypackage/services/CacheTest.java
@@ -1,12 +1,13 @@
 package mypackage.services;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class CacheTest {
 //    pierwszy poziom kesza - persistance context kazdej transakcji
 //  2 ready tego samego nie striggeruja selecta
@@ -14,4 +15,7 @@ public class CacheTest {
 //    drugi - wszystkie transakcje
 
 //    sterowanie (szczegolnie second level) - EhCache z hibernate
+    @Test
+    public void asd () {
+    }
 }
